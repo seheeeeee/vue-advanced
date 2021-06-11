4d9e7f3c08994904f8d60d7a0f385bc37fe2f8b9
LeeSeHee <euc728@naver.com>
2021-06-11 19:05:05 +0900

    AskView, JobsView의 user API 호출

--- vue-advanced/src/api/index.js
+++ vue-advanced/src/api/index.js
@@ -11,6 +11,17 @@ function fetchNewsList(){
   return axios.get(`${config.baseUrl}news/1.json`);
 }
 
+function fetchJobsList(){
+    return axios.get(`${config.baseUrl}jobs/1.json`)
+}
+
+function fetchAskList(){
+    return axios.get(`${config.baseUrl}ask/1.json`)
+}
+
+//3. API 함수 export
 export {
-    fetchNewsList
+    fetchNewsList,
+    fetchJobsList,
+    fetchAskList,
 }
