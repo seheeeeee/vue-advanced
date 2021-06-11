ef8a60939090e17db1c6353f010f891d48accc57
LeeSeHee <euc728@naver.com>
2021-06-11 14:49:17 +0900

    initial version

---
+++ vue-advanced/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <tool-bar></tool-bar>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import ToolBar from './components/ToolBar.vue';
+
+export default {
+ components:{
+   ToolBar,
+ }
+}
+</script>
+
+<style>
+#app {
+  
+}
+</style>
---
+++ vue-advanced/src/router/index.js
@@ -0,0 +1,31 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import NewsView from '../views/NewsView.vue';
+import AskView from '../views/AskView.vue';
+import JobsView from '../views/JobsView.vue';
+
+Vue.use(VueRouter);
+
+export const router = new VueRouter({
+  routes: [
+    {
+      //홈 화면을 바로 news 페이지로 리다이렉트
+      path: '/',
+      redirect: '/news',
+    },
+    {
+        // path: url 이름(주소)
+        path: '/news',
+        // component: url 주소로 갔을 때 표시될 컴포넌트
+        component: NewsView,
+    },
+    {
+        path: '/ask',
+        component: AskView,
+    },
+    {
+        path: '/jobs',
+        component: JobsView,
+    },
+  ]
+});
