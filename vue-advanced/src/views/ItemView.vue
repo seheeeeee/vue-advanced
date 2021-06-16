e34f93925f33ef6873c281cbcc301ba20f2a7add
LeeSeHee <euc728@naver.com>
2021-06-16 16:52:44 +0900

    itemView 스타일링(v-html)

--- vue-advanced/src/views/ItemView.vue
+++ vue-advanced/src/views/ItemView.vue
@@ -1,13 +1,38 @@
 <template>
   <div>
-      <!-- {{ this.$store.state.item }} -->
-      <p>{{ this.$store.state.item.title }}</p>
-      <div>{{ this.$store.state.item.content }}</div>
+    <section>
+      <!-- 질문 상세 정보 -->
+      <div class="user-container">
+        <div>
+          <i class="fas fa-user"></i>
+        </div>
+        <div class="user-description">
+          <router-link v-bind:to="`/user/${fetchedItem.user}`">
+            {{ fetchedItem.user }}
+          </router-link>
+          <div class="time">
+            {{ fetchedItem.time_ago }}
+          </div>
+        </div>
+      </div>
+      <h2>{{ fetchedItem.title }}</h2>
+    </section>
+    <section>
+      <!-- 질문 댓글 -->
+      <div v-html="fetchedItem.content">
+        <!-- {{ fetchedItem.content }} -->
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
 export default {
+  computed: {
+    ...mapGetters(['fetchedItem']),
+  },
   created(){
     const item = this.$route.params.id;
     this.$store.dispatch('FETCH_ITEM', item);
@@ -15,6 +40,19 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.user-container{
+  display: flex;
+  align-items: center;
+  padding: .5rem;
+}
+.fa-user{
+  font-size: 2.5rem;
+}
+.user-description{
+  padding: 8px;
+}
+.time{
+  font-size: 0.7rem;
+}
 </style>
